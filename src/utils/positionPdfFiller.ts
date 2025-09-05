7ff76d3f6fba3076947cd39e985acbc25198d962
habab6 <135975196+habab6@users.noreply.github.com>
2025-09-06 01:08:46 +0200

    Update positionPdfFiller.ts

--- src/utils/positionPdfFiller.ts
+++ src/utils/positionPdfFiller.ts
@@ -9,10 +9,10 @@ const FIELD_POSITIONS = {
   dateDocument: { x: 160, y: 547.5 },          // Position de la date
   civilite: { x: 120, y: 700 },              // Position civilité (M./Mme/Mlle/Mx)
   nomComplet: { x: 210, y: 274.9 },            // Position nom complet
-  dateNaissance: { x: 210, y: 291.1 },         // Position date de naissance
+  dateNaissance: { x: 210, y: 291.2 },         // Position date de naissance
   villeNaissance: { x: 210, y: 650 },        // Position ville de naissance
   paysNaissance: { x: 210, y: 650 },         // Position pays de naissance
-  nationalite: { x: 210, y: 308.7 },           // Position nationalité
+  nationalite: { x: 210, y: 308.4 },           // Position nationalité
   numeroIdentite: { x: 210, y: 324.5 },        // Position numéro d'identité/passeport
   telephone: { x: 210, y: 600 },             // Position téléphone
   email: { x: 210, y: 550 },                 // Position email
