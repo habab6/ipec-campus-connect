46fb80f0952d9547644445a9de1b7ffeb9103348
habab6 <135975196+habab6@users.noreply.github.com>
2025-09-10 16:36:02 +0200

    Update positionPdfFiller.ts

--- src/utils/positionPdfFiller.ts
+++ src/utils/positionPdfFiller.ts
@@ -7,7 +7,7 @@ const ATTESTATION_INSCRIPTION_POSITIONS = {
   numeroDocument: { x: 425, y: 38.5 },       // Position du numéro de document
   dateDocument: { x: 160, y: 547.1 },          // Position de la date
   nomComplet: { x: 210, y: 274.95 },            // Position nom complet
-  dateNaissance: { x: 210, y: 291.7 },         // Position date de naissance
+  dateNaissance: { x: 210, y: 292.3 },         // Position date de naissance
   villeNaissance: { x: 276, y: 291.2 },        // Position ville de naissance
   nationalite: { x: 210, y: 307.5 },           // Position nationalité
   numeroIdentite: { x: 210, y: 323.8 },        // Position numéro d'identité/passeport
